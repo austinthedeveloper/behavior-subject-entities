00354851dd066c5a72a3eceadcf04bc7e84b5d09
Austin Stewart <hello@austinthedeveloper.com>
2020-10-03 14:27:06 -0400

    Removed interfaces

--- src/id-selector.class.ts
+++ src/id-selector.class.ts
@@ -1,5 +1,2 @@
 export type IdSelectorStr<T> = (model: T) => string;
-export type IdSelectorNum<T> = (model: T) => number;
-
 export type IdSelector<T> = IdSelectorStr<T>;
-// export type IdSelector<T> = IdSelectorStr<T> | IdSelectorNum<T>;
