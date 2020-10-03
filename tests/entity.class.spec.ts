9098fc2f6f889aa96a900917531edbf6adc50691
Austin Stewart <hello@austinthedeveloper.com>
2020-10-03 01:11:59 -0400

    More specs

--- tests/entity.class.spec.ts
+++ tests/entity.class.spec.ts
@@ -42,6 +42,18 @@ describe('Entity Class', () => {
       });
     });
 
+    it('should add many items to the items array', done => {
+      entity.addMany([
+        { id: '2', item: { id: '2', name: 'testing' } },
+        { id: '3', item: { id: '3', name: 'testing' } },
+      ]);
+
+      entity.items$.subscribe(items => {
+        expect(items.length).toEqual(3);
+        done();
+      });
+    });
+
     it('should add an item to the items object', done => {
       entity.data$.subscribe(obj => {
         expect(Object.keys(obj).length).toEqual(1);
@@ -53,6 +65,31 @@ describe('Entity Class', () => {
       const snapshot = entity.snapshot;
       expect(snapshot.items.length).toEqual(1);
       expect(Object.keys(snapshot.data).length).toEqual(1);
+    });
+  });
+  describe('Reading Items', () => {
+    let entity: EntityClass<mockInterface>;
+    beforeEach(() => {
+      entity = getClass();
+      entity.addOne({ id: '1', item: { id: '1', name: 'Name 1' } });
+      entity.addOne({ id: '2', item: { id: '2', name: 'Name 2' } });
+      entity.addOne({ id: '3', item: { id: '3', name: 'Name 3' } });
+    });
+
+    it('should retrieve one item', done => {
+      entity.getOne('1').subscribe(item => {
+        expect(entity.idSelector(item)).toEqual('1');
+        done();
+      });
+    });
+    it('should retrieve many items', done => {
+      const arr = ['1', '2', '3'];
+      entity.getMany(arr).subscribe(items => {
+        items.forEach((item, index) => {
+          expect(entity.idSelector(item)).toEqual(arr[index]);
+        });
+        done();
+      });
     });
   });
   describe('Snapshot', () => {
